979f07cf32ec1d3f19e1757ef030c2a582d774ed
eleadn <eleano.dn@gmail.com>
2025-06-01 23:01:53 +0200

    Add display

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+import DisplayHandler from "./displayHandler";
+import getMeteo from "./meteo/getMeteo";
+import MeteoParameters from "./meteo/meteoParameters";
 import "./style.css";
+
+const params = new MeteoParameters(
+	"Paris",
+	"2025-05-26",
+	"2025-06-01",
+	"metric"
+);
+
+const displayHandler = new DisplayHandler(null);
+displayHandler.display();
+
+setTimeout(async () => {
+	const meteo = await getMeteo(params);
+	console.log(meteo.getLocation());
+	displayHandler.updateMeteo(meteo);
+}, 5000);
