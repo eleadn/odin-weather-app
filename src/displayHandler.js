64d8d48e3dd53272944c60980dd7bb18ed7acd44
eleadn <eleano.dn@gmail.com>
2025-06-01 23:01:12 +0200

    Cache current datetime

--- src/displayHandler.js
+++ src/displayHandler.js
@@ -2,10 +2,13 @@ import CurrentDayDisplayer from "./displayer/currentDayDisplayer";
 
 export default class DisplayHandler {
 	#meteo;
+	#currentDatetime;
 	#currentDayDisplayer;
 
 	constructor(meteo) {
 		this.#meteo = meteo;
+		this.#currentDatetime =
+			this.#meteo === null ? null : new Date(Date.now());
 
 		const currentDayContainer = document.querySelector(
 			"#current-day-display"
@@ -18,9 +21,11 @@ export default class DisplayHandler {
 
 	updateMeteo(meteo) {
 		this.#meteo = meteo;
+		this.#currentDatetime = new Date(Date.now());
+		this.display();
 	}
 
 	display() {
-		this.#currentDayDisplayer.show(this.#meteo);
+		this.#currentDayDisplayer.show(this.#meteo, this.#currentDatetime);
 	}
 }
